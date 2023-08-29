52c5109dd5ce8c29a73e4c7382225266504e09d6
dineshpinto <annual.fallout_0z@icloud.com>
2023-08-29 20:57:51 +0200

    Updated test suite

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -16,6 +16,9 @@ class TestExceptions(TestCase):
             self.cg.open_interest_history(symbol="BTC", time_type="m1", currency="USD")
             self.cg.funding_usd_history(symbol="BTC", time_type="m1")
             self.cg.funding_coin_history(symbol="BTC", time_type="m1")
+            self.cg.liquidation_map(symbol="Binance_BTCUSDT", interval="1d")
+            self.cg.liquidation_order(ex_name="Binance", coin="BTC", vol_usd="1000",
+                                      start_time=1693323421369, end_time=1693333421369)
 
     def test_no_data_returned_exception(self) -> None:
         with self.assertRaises(NoDataReturnedException):
@@ -23,27 +26,17 @@ class TestExceptions(TestCase):
 
     def test_warning_message(self) -> None:
         with self.assertWarns(CoinglassParameterWarning):
-            with self.assertRaises(NoDataReturnedException):
-                self.cg.futures_market(symbol="NotASymbol")
+            with self.assertRaises(CoinglassRequestException):
+                self.cg.open_interest_ohlc(ex="NotAnExchange", pair="BTCUSDT", interval="h1")
 
     def test_exchanges(self) -> None:
         exchanges = self.cg.get_exchanges()
         self.assertIn("Deribit", exchanges)
         self.assertIn("OKX", exchanges)
-        self.assertIn("CME", exchanges)
 
         self.cg.add_exchange("TestExchange")
         exchanges = self.cg.get_exchanges()
         self.assertIn("TestExchange", exchanges)
-
-    def test_symbols(self) -> None:
-        symbols = self.cg.get_symbols()
-        self.assertIn("ETH", symbols)
-        self.assertIn("BTC", symbols)
-
-        self.cg.add_symbol("TestSymbol")
-        symbols = self.cg.get_symbols()
-        self.assertIn("TestSymbol", symbols)
 
     def test_time_types(self) -> None:
         time_types = self.cg.get_time_types()
